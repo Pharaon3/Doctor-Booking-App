16e79ab98ae0fc0cc0f0f1ef662fef27aa8a0678
Pharaon3 <Pharaon313@outlook.com>
2023-02-09 03:44:21 +0300

    CAPABILITIES three grid

--- src/components/capability.jsx
+++ src/components/capability.jsx
@@ -1,17 +1,42 @@
 import React from "react";
+import { MDBRow, MDBCol } from "mdb-react-ui-kit";
 
 export const Capability = (props) => {
   return (
     <div id="capability">
       <div className="container">
         <div className="row">
-          <h4 style={{textAlign: "center"}}> CORE CAPABILITIES </h4>
-          <h2 style={{textAlign: "center"}}> I forcus on </h2>
-          <div style={{display: "flex"}}>
-            <div className="roundDiv">
-              kak
-            </div>
-          </div>
+          <h4 style={{ textAlign: "center" }}> CORE CAPABILITIES </h4>
+          <h2 style={{ textAlign: "center" }}> I forcus on </h2>
+          <MDBRow>
+            <MDBCol md="4">
+              <div className="roundDiv">
+                <img
+                  src={props.data ? props.data.leftImage : "none"}
+                  className=""
+                  alt="left image"
+                />
+              </div>
+            </MDBCol>
+            <MDBCol md="4">
+              <div className="roundDiv">
+                <img
+                  src={props.data ? props.data.leftImage : "none"}
+                  className=""
+                  alt="left image"
+                />
+              </div>
+            </MDBCol>
+            <MDBCol md="4">
+              <div className="roundDiv">
+                <img
+                  src={props.data ? props.data.leftImage : "none"}
+                  className=""
+                  alt="left image"
+                />
+              </div>
+            </MDBCol>
+          </MDBRow>
         </div>
       </div>
     </div>
