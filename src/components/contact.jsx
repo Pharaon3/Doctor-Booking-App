6b6f72b59812ef523955a7658a73cfd00d4ed7da
Pharaon3 <Pharaon313@outlook.com>
2023-02-09 23:56:37 +0300

    contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -35,91 +35,55 @@ export const Contact = (props) => {
     <div>
       <div id="contact">
         <div className="container">
-          <div className="col-md-8">
-            <div className="row">
-              <div className="section-title">
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
+          <div className="col-md-3"></div>
+          <div class="stacked-title-2 col-md-6">
+            <div class="color-white subtitle">join the converation</div>
+            <h1 class="heading-3 text-white">
+              Stay on the leading edge of functional health news, biohacking
+              tips and learn with me each month.
+            </h1>
+
+            <form name="sentMessage" validate onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="form-control"
+                      placeholder="Name"
+                      required
+                      onChange={handleChange}
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
+                </div>
+                <div className="col-md-2"></div>
               </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
+              <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Email"
+                      required
+                      onChange={handleChange}
+                    />
+                    <p className="help-block text-danger"></p>
                   </div>
                 </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
-                </button>
-              </form>
-            </div>
-          </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Contact Info</h3>
-              <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Address
-                </span>
-                {props.data ? props.data.address : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone"></i> Phone
-                </span>{" "}
-                {props.data ? props.data.phone : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>
+              </div>
+              <div id="success"></div>
+              <button type="submit" className="btn btn-custom btn-lg">
+                Submit
+              </button>
+            </form>
           </div>
           <div className="col-md-12">
             <div className="row">
@@ -149,9 +113,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2021 DR ALEX CHAN{" "}
+            <a href="https://github.com/Pharaon3/Doctor-Booking-App" rel="nofollow">
+            PRIVACY POLICY
             </a>
           </p>
         </div>
