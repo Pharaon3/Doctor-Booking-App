465581d11fd2d24eb26f81eed9fc09ef9370d34c
Pharaon3 <Pharaon313@outlook.com>
2023-02-10 00:21:20 +0300

    header fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,27 +7,29 @@ export const Header = (props) => {
         <div className="overlay">
           <div className="container">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h2>
+              <div className="col-md-10 col-md-offset-1 intro-text">
+                <h2 style={{ fontSize: 56 }}>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h2>{" "}
               </div>
             </div>
-              <div style={{ display: "flex" }}>
-                <div className="hero-left">
-                  <h3> {props.data ? props.data.leftTitle : "Loading"}</h3>
-                  <p className="leftContent">
-                    {" "}
-                    {props.data ? props.data.leftContent : "Loading"}{" "}
-                  </p>
-                </div>
-                <img
-                  src={props.data ? props.data.largeImage : "none"}
-                  className="mainImage"
-                  alt={props.data ? props.data.title : "Loading"}
-                />
+          </div>
+          <div className="row col-md-8 col-md-offset-1">
+            <div style={{ display: "flex" }}>
+              <div className="hero-left col-md-3">
+                <h3> {props.data ? props.data.leftTitle : "Loading"}</h3>
+                <p className="leftContent" style={{ fontWeight: "normal" }}>
+                  {" "}
+                  {props.data ? props.data.leftContent : "Loading"}{" "}
+                </p>
               </div>
+              <img
+                src={props.data ? props.data.largeImage : "none"}
+                className="mainImage col-md-6 col-md-offset-1"
+                alt={props.data ? props.data.title : "Loading"}
+              />
+            </div>
           </div>
         </div>
       </div>
